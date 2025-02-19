04016c2201f833adbf3e5cc4b5157fe7cec21a47
Manojkumar <116948057+Vmkmanoj@users.noreply.github.com>
2025-02-19 20:04:53 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_jwt_extended import create_access_token, JWTManager
+from flask_cors import CORS
+import psycopg2.pool
+from datetime import date
 
 app = Flask(__name__)
+CORS(app)
+
+# Database connection pool
+DATABASE_URL = "postgresql://neondb_owner:npg_vw3df1GEKtgD@ep-shy-tooth-a5qd2qrz-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+conn_pool = psycopg2.pool.SimpleConnectionPool(1, 10, DATABASE_URL)
+
+app.config["JWT_SECRET_KEY"] = "your_secret_key"
+jwt = JWTManager(app)
+
+def get_db_connection():
+    return conn_pool.getconn()
 
 @app.route('/')
 def home():
